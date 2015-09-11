2fdca96c2ac35ace27c8ec38c00167596d601ae5
Ole Kristian Aune <okaune@outlook.com>
2015-09-11 10:37:31 +0200

    Lagt til grunnkde i Main.java

---
+++ Main.java
@@ -0,0 +1,26 @@
+import lejos.hardware.motor.*;
+import lejos.hardware.lcd.*;
+import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.hardware.sensor.EV3GyroSensor;
+import lejos.hardware.sensor.NXTTouchSensor;
+import lejos.hardware.sensor.NXTLightSensor;
+import lejos.hardware.sensor.NXTColorSensor;
+import lejos.hardware.sensor.NXTSoundSensor;
+import lejos.hardware.sensor.NXTUltrasonicSensor;
+import lejos.hardware.port.Port;
+import lejos.hardware.Brick;
+import lejos.hardware.BrickFinder;
+import lejos.hardware.ev3.EV3;
+import lejos.hardware.Keys;
+import lejos.hardware.sensor.SensorModes;
+import lejos.robotics.SampleProvider;
+import lejos.hardware.sensor.*;
+
+
+public class Main{
+	public static void main (String[] arg) throws Exception  {
+	
+	}
+}
